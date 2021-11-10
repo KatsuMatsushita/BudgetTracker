28132429ede62fb678634a7eed683449c31589f6
Hungry Man <katsu.matsushita@gmail.com>
2021-11-09 22:32:50 -0500

    Fixed so that page keeps cached data. Index.js and service-worker.js fixed so that transactions stay when offline and page refreshed.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,8 @@
 //boilerplate install, caching, and activation of service-worker taken from class activities
-
+//CACHE_NAME is for caching the files for offline use
 const CACHE_NAME = "static-cache-v2";
-//const DATA_CACHE_NAME = "data-cache-v1";
+// DATA_CACHE_NAME is for caching any api requests while the app is offline
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -14,10 +15,6 @@ const FILES_TO_CACHE = [
 
 // install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
-    // evt.waitUntil(
-    //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-    // );
     
   // pre cache all static assets
   evt.waitUntil(
@@ -49,33 +46,41 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", function(evt) {
-//   if (evt.request.url.includes("/api/")) {
-//     evt.respondWith(
-//       caches.open(CACHE_NAME).then(cache => {
-//         return fetch(evt.request)
-//           .then(response => {
-//             // If the response was good, clone it and store it in the cache.
-//             if (response.status === 200) {
-//               cache.put(evt.request.url, response.clone());
-//             }
+    // this will cache the requests that are sent to the /api routes
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
-//             return response;
-//           })
-//           .catch(err => {
-//             // Network request failed, try to get it from the cache.
-//             return cache.match(evt.request);
-//           });
-//       }).catch(err => console.log(err))
-//     );
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
 
-//     return;
-//   }
-
+    return;
+  }
+  // this event first attempts to fetch the page
+  // if it fails (such as if its offline), then it will return the home page from the cache
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
-        return response || fetch(evt.request);
-      });
+    fetch(evt.request)
+    .catch(() => {
+        return caches.match(evt.request)
+            .then( (response) => {
+                if(response) {
+                    return response;
+                } else if (evt.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/");
+                }
+            })
     })
   );
 });
